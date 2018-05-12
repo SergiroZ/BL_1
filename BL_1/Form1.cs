0093814463487ea6ba5280bcdf1ace287bbe225c
Sergiro Z <zujonok@hotmail.com>
2018-05-13 01:05:53 +0300

    12-05

--- BL_1/Form1.cs
+++ BL_1/Form1.cs
@@ -15,6 +15,44 @@ namespace BL_1
         public Form1()
         {
             InitializeComponent();
+
+            GetAllBooks();
+        }
+
+        private void GetAllBooks()
+        {
+            using (var context = new LibraryEntities())
+            {
+                var query = from books in context.Books
+                            join author in context.Authors on books.IdAuthor equals author.Id
+                            join publisher in context.Publishers on books.IdPublisher equals publisher.Id
+                            select new
+                            {
+                                books.Id,
+                                aFName = author.FirstName,
+                                aLName = author.LastName,
+                                books.Title,
+                                books.Pages,
+                                books.Price,
+                                publisher.PublisherName,
+                                publisher.Address
+                            };
+                dataGridView1.DataSource = query.ToList();
+                dataGridView1.Columns["aFName"].HeaderText = "First name";
+                dataGridView1.Columns["aLName"].HeaderText = "Second name";
+                dataGridView1.Columns["PublisherName"].HeaderText = "Publisher name";
+            }
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            //try
+            //{
+            //}
+            //catch (Exception)
+            //{
+            //    //throw;
+            //}
         }
     }
 }
